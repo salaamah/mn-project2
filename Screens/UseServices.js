9a4dc3c117a4eaf1f4c54c6b2ca84f8b36ee55cc
salaamah <muhammadsalamatullah@gmail.com>
2022-05-25 06:47:19 +0300

    expo updated

---
+++ Screens/UseServices.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const UseServices = () => {
+  return (
+    <View>
+      <Text>UseServices</Text>
+    </View>
+  )
+}
+
+export default UseServices
+
+const styles = StyleSheet.create({})
