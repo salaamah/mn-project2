6709f50600f62c17a52a7c59d164f950dd20ef18
salaamah <muhammadsalamatullah@gmail.com>
2022-09-19 23:06:01 +0300

    api added for prayer times

--- Screens/PrayerTimes.js
+++ Screens/PrayerTimes.js
@@ -1,10 +1,20 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const PrayerTimes = () => {
+  const [timeTable, setTimeTable] = useState({});
+
+  useEffect( ()=>{
+    fetch('https://api.aladhan.com/v1/timingsByCity?city=Madinah&country=Saudi%20Arabia%20&method=4')
+    .then(res=>res.json())
+    .then(data=>setTimeTable(data.data.timings))
+  },[]);
+
+  const Fazr = timeTable.Fajr;
   return (
     <View>
       <Text>Prayer Times in Madinah</Text>
+      <Text>Fazr: {Fazr}</Text>
     </View>
   )
 }
